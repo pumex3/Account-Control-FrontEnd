7b593de5dce03241cbc5a7c41f3510ca9e2924b8
pedro_aleixo22@hotmail.com <pedro_aleixo22@hotmail.com>
2020-05-25 15:09:12 -0300

    devpt2

--- src/main/dashboard/dashboard.jsx
+++ src/main/dashboard/dashboard.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default proips => (
-    <div>
-        <h1>DashROLA</h1>
-    </div>
-)
+import ContentHeader from '../../common/template/contentHeader'
+import Content from '../../common/template/content'
+import ValueBox from '../../common/widget/valueBox'
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Dashboard' small='Versão 1.0' />
+                <Content >
+                    <ValueBox cols=' 12 4' color='green' icon='bank'
+                        value='R$ 10' text='Total de Créditos' />
+
+                    <ValueBox cols=' 12 4' color='red' icon='credit-card'
+                        value='R$ 10' text='Total de Débitos' />
+
+                    <ValueBox cols=' 12 4' color='blue' icon='money'
+                        value='R$ 0' text='Valor Consolidado' />
+
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default Dashboard
